f2eb6fb20084ccbe008f63ba738c881dbf640fcf
SalamunFajri <salcad@yahoo.com>
2021-06-13 18:19:23 +0700

    Initial test3

---
+++ test3/src/com/company/Main.java
@@ -0,0 +1,88 @@
+package com.company;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+
+    static int MAX = 1000001;
+    static int factor[];
+
+    // generate all prime factors of numbers from 1 to 1 million
+    static void genPrimeFactor()
+    {
+        factor[1] = 1;
+
+        for (int i = 2; i < MAX; i++)
+            factor[i] = i;
+
+        for (int i = 4; i < MAX; i += 2)
+            factor[i] = 2;
+
+        for (int i = 3; i * i < MAX; i++) {
+            if (factor[i] == i) {
+                for (int j = i * i; j < MAX; j += i) {
+                    if (factor[j] == j)
+                        factor[j] = i;
+                }
+            }
+        }
+    }
+
+    static int calcNumsOfFactors(int n)
+    {
+        if (n == 1)
+            return 1;
+
+        int result = 1;
+        int smallestPrimeNumberThatDividesN = factor[n];
+        int countNumsOfPrimesDividesN = 1;
+        int j = n / factor[n];
+
+        while (j != 1) {
+            if (factor[j] == smallestPrimeNumberThatDividesN)
+                countNumsOfPrimesDividesN += 1;
+            else {
+                smallestPrimeNumberThatDividesN = factor[j];
+                result = result * (countNumsOfPrimesDividesN + 1);
+                countNumsOfPrimesDividesN = 1;
+            }
+
+            j = j / factor[j];
+        }
+
+        result = result * (countNumsOfPrimesDividesN + 1);
+        return result;
+    }
+
+    private static int getNumberWith6SixFactor(int input) {
+        int[] arr = new int[input];
+        for(int i=0; i<arr.length; i++) {
+            arr[i] = i+1;
+        }
+        int numsOf6Factor = 0;
+        for (int i=0; i<arr.length; i++) {
+            if (calcNumsOfFactors(arr[i]) == 6) {
+                numsOf6Factor++;
+            }
+        }
+        return numsOf6Factor;
+    }
+
+    public static void main(String[] args)
+    {
+        String strN = null;
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(System.in))) {
+            strN = br.readLine();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        factor = new int[MAX];
+        factor[0] = 0;
+        genPrimeFactor();
+
+        System.out.printf("Total angka yang tidak melebihi n dan dengan tepat memiliki 6 faktor: %d%n", getNumberWith6SixFactor(Integer.parseInt(strN)));
+    }
+
+}
